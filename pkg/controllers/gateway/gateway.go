5dbf85c67a13656f05b59ddaa38f0f274845764a
Darren Shepherd <darren@rancher.com>
2019-05-20 17:54:41 -0700

    Remove endpoint finalizer

--- pkg/controllers/gateway/gateway.go
+++ pkg/controllers/gateway/gateway.go
@@ -6,6 +6,7 @@ import (
 	"sync"
 
 	"github.com/rancher/rio-autoscaler/types"
+	"github.com/rancher/wrangler/pkg/kv"
 	"github.com/sirupsen/logrus"
 	corev1 "k8s.io/api/core/v1"
 )
@@ -18,7 +19,6 @@ func Register(ctx context.Context, rContext *types.Context) error {
 	logrus.Info("Starting gateway endpoint controller")
 	e := endpointHandler{}
 	rContext.Core.Core().V1().Endpoints().OnChange(ctx, "gateway-endpoint-watcher", e.Sync)
-	rContext.Core.Core().V1().Endpoints().OnRemove(ctx, "gateway-endpoint-watcher", e.Remove)
 
 	return nil
 }
@@ -26,6 +26,12 @@ func Register(ctx context.Context, rContext *types.Context) error {
 type endpointHandler struct{}
 
 func (e endpointHandler) Sync(key string, obj *corev1.Endpoints) (*corev1.Endpoints, error) {
+	if obj == nil {
+		namespace, name := kv.Split(key, "/")
+		EndpointChanMap.Delete(fmt.Sprintf("%s.%s", name, namespace))
+		return nil, nil
+	}
+
 	// todo: add a filter only for scale-to-zero services so that we don't have to keep a channel for every endpoint
 	if obj != nil && obj.DeletionTimestamp == nil {
 		ch := make(chan struct{}, 0)
@@ -53,11 +59,4 @@ func isEndpointReady(obj *corev1.Endpoints) bool {
 		}
 	}
 	return ready
-}
-
-func (e endpointHandler) Remove(key string, obj *corev1.Endpoints) (*corev1.Endpoints, error) {
-	if obj != nil {
-		EndpointChanMap.Delete(fmt.Sprintf("%s.%s", obj.Name, obj.Namespace))
-	}
-	return obj, nil
 }
